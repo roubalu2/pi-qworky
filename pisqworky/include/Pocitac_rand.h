15e89e5cfb5465f63b9bc0e6825449f8c5fca0f9
MarekBelda <91273800+MarekBelda@users.noreply.github.com>
2021-11-13 17:15:12 +0100

    Generator nahodnych cisel 01

---
+++ pisqworky/include/Pocitac_rand.h
@@ -0,0 +1,23 @@
+#ifndef POCITAC_RAND_H
+#define POCITAC_RAND_H
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include "Deska.h"
+
+using namespace std;
+
+class Pocitac_rand
+{
+    public:
+        Pocitac_rand();
+        vector<int> tah(Deska &);
+
+
+    protected:
+
+    private:
+};
+
+#endif // POCITAC_RAND_H
