c354a0bacd451d2a5c2b19e678e3268f2ed74e7c
roubalu2 <91847306+roubalu2@users.noreply.github.com>
2021-11-14 12:09:02 +0100

    LR04
    Snad většina desky

--- pisqworky/include/Deska.h
+++ pisqworky/include/Deska.h
@@ -6,21 +6,30 @@
 using namespace std;
 
 const int VelPol = 15; //pole 15x15
-
+const int VelVyh = 5;
 
 class Deska
 {
-    public:
-        Deska();
-        void clear();
-        void print();
-        bool prazdne(int, int);
-    protected:
+public:
+    Deska();
+    void clear();
+    void print();
+    bool prazdne(int, int);
+    void hodnota(int, int, char);
+    char nacti (int, int);
+    bool vyhra(int, int);
+    int pocitej();
+    bool radek(int, int);
+    bool sloupec(int, int);
+    bool diagonala1(int, int);
+    bool diagonala2(int, int);
+string toString();
+protected:
 
-    private:
-        char deska [VelPol][VelPol];
-        int count;
-        void Hodnota(int, int, char);
+private:
+    char deska [VelPol][VelPol];
+    int count;
+
 
 
 };
