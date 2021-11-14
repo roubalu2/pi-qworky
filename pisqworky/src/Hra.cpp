17f1430e7f3597c1d95cd41c86eb72847646d0c2
Silinluc <lucie.silingova@fs.cvut.cz>
2021-11-14 18:34:09 +0100

    LS
    Upravy.

--- pisqworky/src/Hra.cpp
+++ pisqworky/src/Hra.cpp
@@ -77,132 +77,132 @@ void Hra::Konzole ()
 
             }
 
-/*
-            if(souradnice.find("14")==true)
-            {
-                x=14;
-            }
-            else if (souradnice.find("13")==true)
-            {
-                x=13;
-            }
-            else if (souradnice.find("12")==true)
-            {
-                x=12;
-            }
-            else if (souradnice.find("11")==true)
-            {
-                x=11;
-            }
-            else if (souradnice.find("10")==true)
-            {
-                x=10;
-            }
-            else if (souradnice.find("9")==true)
-            {
-                x=9;
-            }
-            else if (souradnice.find("8")==true)
-            {
-                x=8;
-            }
-            else if (souradnice.find("7")==true)
-            {
-                x=7;
-            }
-            else if (souradnice.find("6")==true)
-            {
-                x=6;
-            }
-            else if (souradnice.find("5")==true)
-            {
-                x=5;
-            }
-            else if (souradnice.find("4")==true)
-            {
-                x=4;
-            }
-            else if (souradnice.find("3")==true)
-            {
-                x=3;
-            }
-            else if (souradnice.find("2")==true)
-            {
-                x=2;
-            }
-            else if (souradnice.find("1")==true)
-            {
-                x=1;
-            }
-            else if (souradnice.find("0")==true)
-            {
-                x=0;
-            }
+            /*
+                        if(souradnice.find("14")==true)
+                        {
+                            x=14;
+                        }
+                        else if (souradnice.find("13")==true)
+                        {
+                            x=13;
+                        }
+                        else if (souradnice.find("12")==true)
+                        {
+                            x=12;
+                        }
+                        else if (souradnice.find("11")==true)
+                        {
+                            x=11;
+                        }
+                        else if (souradnice.find("10")==true)
+                        {
+                            x=10;
+                        }
+                        else if (souradnice.find("9")==true)
+                        {
+                            x=9;
+                        }
+                        else if (souradnice.find("8")==true)
+                        {
+                            x=8;
+                        }
+                        else if (souradnice.find("7")==true)
+                        {
+                            x=7;
+                        }
+                        else if (souradnice.find("6")==true)
+                        {
+                            x=6;
+                        }
+                        else if (souradnice.find("5")==true)
+                        {
+                            x=5;
+                        }
+                        else if (souradnice.find("4")==true)
+                        {
+                            x=4;
+                        }
+                        else if (souradnice.find("3")==true)
+                        {
+                            x=3;
+                        }
+                        else if (souradnice.find("2")==true)
+                        {
+                            x=2;
+                        }
+                        else if (souradnice.find("1")==true)
+                        {
+                            x=1;
+                        }
+                        else if (souradnice.find("0")==true)
+                        {
+                            x=0;
+                        }
 
-             if (souradnice.find("A") != std::string::npos || souradnice.find ("a") != std::string::npos)
-            {
-                y=0;
-            }
-            else if (souradnice.find("B") ==true || souradnice.find ("b")==true)
-            {
-                y=1;
-            }
-            else if (souradnice.find("C") ==true|| souradnice.find ("c")==true)
-            {
-                y=2;
-            }
-            else if (souradnice.find("D")==true || souradnice.find ("d")==true)
-            {
-                y=3;
-            }
-            else if (souradnice.find("E") ==true || souradnice.find ("e")==true)
-            {
-                y=4;
-            }
-            else if (souradnice.find("F")==true || souradnice.find ("f")==true)
-            {
-                y=5;
-            }
-            else if (souradnice.find("G")==true || souradnice.find ("g")==true)
-            {
-                y=6;
-            }
-            else if (souradnice.find("H") ==true || souradnice.find ("h")==true)
-            {
-                y=7;
-            }
-            else if (souradnice.find("I")==true || souradnice.find ("i")==true)
-            {
-                y=8;
-            }
-            else if (souradnice.find("J")==true || souradnice.find ("j")==true)
-            {
-                y=9;
-            }
-            else if (souradnice.find("K") ==true|| souradnice.find ("k")==true)
-            {
-                y=10;
-            }
-            else if  (souradnice.find("L")==true || souradnice.find ("l")==true)
-            {
-                y=11;
-            }
-            else if (souradnice.find("M")==true || souradnice.find ("m")==true)
-            {
-                y=12;
-            }
-            else if (souradnice.find("N") ==true|| souradnice.find ("n")==true)
-            {
-                y=13;
-            }
-            else if (souradnice.find("O")==true || souradnice.find ("o")==true);
-            {
-                y=14;
-            }
+                         if (souradnice.find("A") != std::string::npos || souradnice.find ("a") != std::string::npos)
+                        {
+                            y=0;
+                        }
+                        else if (souradnice.find("B") ==true || souradnice.find ("b")==true)
+                        {
+                            y=1;
+                        }
+                        else if (souradnice.find("C") ==true|| souradnice.find ("c")==true)
+                        {
+                            y=2;
+                        }
+                        else if (souradnice.find("D")==true || souradnice.find ("d")==true)
+                        {
+                            y=3;
+                        }
+                        else if (souradnice.find("E") ==true || souradnice.find ("e")==true)
+                        {
+                            y=4;
+                        }
+                        else if (souradnice.find("F")==true || souradnice.find ("f")==true)
+                        {
+                            y=5;
+                        }
+                        else if (souradnice.find("G")==true || souradnice.find ("g")==true)
+                        {
+                            y=6;
+                        }
+                        else if (souradnice.find("H") ==true || souradnice.find ("h")==true)
+                        {
+                            y=7;
+                        }
+                        else if (souradnice.find("I")==true || souradnice.find ("i")==true)
+                        {
+                            y=8;
+                        }
+                        else if (souradnice.find("J")==true || souradnice.find ("j")==true)
+                        {
+                            y=9;
+                        }
+                        else if (souradnice.find("K") ==true|| souradnice.find ("k")==true)
+                        {
+                            y=10;
+                        }
+                        else if  (souradnice.find("L")==true || souradnice.find ("l")==true)
+                        {
+                            y=11;
+                        }
+                        else if (souradnice.find("M")==true || souradnice.find ("m")==true)
+                        {
+                            y=12;
+                        }
+                        else if (souradnice.find("N") ==true|| souradnice.find ("n")==true)
+                        {
+                            y=13;
+                        }
+                        else if (souradnice.find("O")==true || souradnice.find ("o")==true);
+                        {
+                            y=14;
+                        }
 
-            cout << souradnice << "   wtf ?" << endl;
-*/
-cout << "X: "<<x <<"   Y: "<< y << endl;
+                        cout << souradnice << "   wtf ?" << endl;
+            */
+
         }
         else
         {
@@ -237,11 +237,29 @@ cout << "X: "<<x <<"   Y: "<< y << endl;
                 cout << "Souradnice tahu pocitace jsou: ("<<x<<", "<<y<<") "<<endl;
             }
         }
+//kontrola vyhry
+        if (deska.vyhra(x,y))
+        {
+            deska.print();
+            if (hod == 1)
+            {
+                cout<<"Gratuluji! Vyhral jste!"<<endl;
 
-        hod= 3 - hod;
+            }
+            else
+            {
+                cout<< "Pocitac vyhral!"<<endl;
+            }
+            break;
+        }
+        else
+        {
+            hod= 3 - hod;
+        }
+
+
+
     }
-
-
 
 
 }
@@ -264,18 +282,19 @@ char Hra::nactiSymbol(int hod)
 void Hra::spustit()
 {
     bool flag = true;
-    char x;
+    string x;
     while (flag)
     {
         Konzole();
         cout << "Prejete si hrat dalsi hru? ano/ne"<<endl;
         cin >> x;
-        if (x == 'N' || x == 'n')
+        if (x == "ano")
         {
-            flag = false;
+            flag = true;
         }
         else
         {
+            flag = false;
             cout << "------------------------------------------------------"<<endl;
         }
     }
