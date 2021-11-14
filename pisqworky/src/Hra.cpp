ec0307e06aa7e9c656b2fd39712bbc062242f73a
roubalu2 <91847306+roubalu2@users.noreply.github.com>
2021-11-14 16:06:37 +0100

    LR04
    funguje zadavani hrace

--- pisqworky/src/Hra.cpp
+++ pisqworky/src/Hra.cpp
@@ -4,6 +4,9 @@
 #include <vector>
 #include <iostream>
 
+
+#include <regex>
+
 using namespace std;
 
 Hra::Hra()
@@ -42,71 +45,39 @@ void Hra::Konzole ()
     while (flag)
     {
         cout << endl;
+
         if (hod == 1)
         {
             cout << "Jste na tahu.\nZadejte souradnice vaseho tahu." << endl;
             string souradnice;
             cin >> souradnice;
-            if (souradnice.find("A") ==true || souradnice.find ("a") ==true )
+            string data2= souradnice;
+
+            string xreg;
+            string yreg;
+
+            std::regex tah("^\\d+\\w$|^\\w\\d+$");
+            if(std::regex_match(souradnice, tah)==1)
             {
-                y=0;
+                std::regex cislo("\\d+");
+                std::regex pismeno("\\w");
+                smatch cislomatch;
+                smatch pismenomatch;
+                regex_search(souradnice,cislomatch,cislo);
+                regex_search(souradnice,pismenomatch,pismeno);
+                for(auto i:cislomatch)
+                    xreg=i;
+                for(auto i:pismenomatch)
+                    yreg = i;
+
+
+                x= atoi(xreg.c_str());
+                char ychar =tolower(yreg.c_str()[0]);
+                y=ychar-97;
+
             }
-            else if (souradnice.find("B") ==true || souradnice.find ("b")==true)
-            {
-                y=1;
-            }
-            else if (souradnice.find("C") ==true|| souradnice.find ("c")==true)
-            {
-                y=2;
-            }
-            else if (souradnice.find("D")==true || souradnice.find ("d")==true)
-            {
-                y=3;
-            }
-            else if (souradnice.find("E") ==true || souradnice.find ("e")==true)
-            {
-                y=4;
-            }
-            else if (souradnice.find("F")==true || souradnice.find ("f")==true)
-            {
-                y=5;
-            }
-            else if (souradnice.find("G")==true || souradnice.find ("g")==true)
-            {
-                y=6;
-            }
-            else if (souradnice.find("H") ==true || souradnice.find ("h")==true)
-            {
-                y=7;
-            }
-            else if (souradnice.find("I")==true || souradnice.find ("i")==true)
-            {
-                y=8;
-            }
-            else if (souradnice.find("J")==true || souradnice.find ("j")==true)
-            {
-                y=9;
-            }
-            else if (souradnice.find("K") ==true|| souradnice.find ("k")==true)
-            {
-                y=10;
-            }
-            else if  (souradnice.find("L")==true || souradnice.find ("l")==true)
-            {
-                y=11;
-            }
-            else if (souradnice.find("M")==true || souradnice.find ("m")==true)
-            {
-                y=12;
-            }
-            else if (souradnice.find("N") ==true|| souradnice.find ("n")==true)
-            {
-                y=13;
-            }
-            else (souradnice.find("O")==true || souradnice.find ("o")==true);
-            {
-                y=14;
-            }
+
+/*
             if(souradnice.find("14")==true)
             {
                 x=14;
@@ -167,13 +138,81 @@ void Hra::Konzole ()
             {
                 x=0;
             }
+
+             if (souradnice.find("A") != std::string::npos || souradnice.find ("a") != std::string::npos)
+            {
+                y=0;
+            }
+            else if (souradnice.find("B") ==true || souradnice.find ("b")==true)
+            {
+                y=1;
+            }
+            else if (souradnice.find("C") ==true|| souradnice.find ("c")==true)
+            {
+                y=2;
+            }
+            else if (souradnice.find("D")==true || souradnice.find ("d")==true)
+            {
+                y=3;
+            }
+            else if (souradnice.find("E") ==true || souradnice.find ("e")==true)
+            {
+                y=4;
+            }
+            else if (souradnice.find("F")==true || souradnice.find ("f")==true)
+            {
+                y=5;
+            }
+            else if (souradnice.find("G")==true || souradnice.find ("g")==true)
+            {
+                y=6;
+            }
+            else if (souradnice.find("H") ==true || souradnice.find ("h")==true)
+            {
+                y=7;
+            }
+            else if (souradnice.find("I")==true || souradnice.find ("i")==true)
+            {
+                y=8;
+            }
+            else if (souradnice.find("J")==true || souradnice.find ("j")==true)
+            {
+                y=9;
+            }
+            else if (souradnice.find("K") ==true|| souradnice.find ("k")==true)
+            {
+                y=10;
+            }
+            else if  (souradnice.find("L")==true || souradnice.find ("l")==true)
+            {
+                y=11;
+            }
+            else if (souradnice.find("M")==true || souradnice.find ("m")==true)
+            {
+                y=12;
+            }
+            else if (souradnice.find("N") ==true|| souradnice.find ("n")==true)
+            {
+                y=13;
+            }
+            else if (souradnice.find("O")==true || souradnice.find ("o")==true);
+            {
+                y=14;
+            }
+
+            cout << souradnice << "   wtf ?" << endl;
+*/
+cout << "X: "<<x <<"   Y: "<< y << endl;
         }
         else
         {
 
             v = pocitac_rand.tah(deska);
+
             x = v[0];
             y = v[1];
+
+
 
         }
 
@@ -198,7 +237,8 @@ void Hra::Konzole ()
                 cout << "Souradnice tahu pocitace jsou: ("<<x<<", "<<y<<") "<<endl;
             }
         }
-    hod= 3 - hod;
+
+        hod= 3 - hod;
     }
 
 
@@ -221,19 +261,24 @@ char Hra::nactiSymbol(int hod)
         return O;
     }
 }
-void Hra::spustit(){
-	bool flag = true;
-	char x;
-	while (flag){
-		Konzole();
-		cout << "Prejete si hrat dalsi hru? ano/ne"<<endl;
-		cin >> x;
-		if (x == 'N' || x == 'n'){
-			flag = false;
-		}else{
-			cout << "------------------------------------------------------"<<endl;
-		}
-	}
+void Hra::spustit()
+{
+    bool flag = true;
+    char x;
+    while (flag)
+    {
+        Konzole();
+        cout << "Prejete si hrat dalsi hru? ano/ne"<<endl;
+        cin >> x;
+        if (x == 'N' || x == 'n')
+        {
+            flag = false;
+        }
+        else
+        {
+            cout << "------------------------------------------------------"<<endl;
+        }
+    }
 }
 
 
