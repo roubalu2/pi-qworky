48ffce4553671770cf4a4eb10aa62428ecc19ad7
Silinluc <lucie.silingova@fs.cvut.cz>
2021-11-14 13:20:15 +0100

    LS
    Oprava souradnic.

--- pisqworky/src/Hra.cpp
+++ pisqworky/src/Hra.cpp
@@ -23,7 +23,7 @@ void Hra::Konzole ()
     //deska.hracSymbol ('X');
     //deska.pocitacSymbol ('O');
 
-    cout << "Vas symbol je X./nPrejete si hrat jako prvni? ano/ne" << endl;
+    cout << "Vas symbol je X.\nPrejete si hrat jako prvni? ano/ne" << endl;
     string poradi;
     cin >> poradi;
 
@@ -44,128 +44,128 @@ void Hra::Konzole ()
         cout << endl;
         if (hod == 1)
         {
-            cout << "Jste na tahu./nZadejte souradnice vaseho tahu." << endl;
+            cout << "Jste na tahu.\nZadejte souradnice vaseho tahu." << endl;
             string souradnice;
             cin >> souradnice;
-            if (souradnice.find("A") || souradnice.find ("a") )
+            if (souradnice.find("A") ==true || souradnice.find ("a") ==true )
             {
-                x=0;
+                y=0;
             }
-            else if (souradnice.find("B") || souradnice.find ("b"))
-            {
-                x=1;
-            }
-            else if (souradnice.find("C") || souradnice.find ("c"))
-            {
-                x=2;
-            }
-            else if (souradnice.find("D") || souradnice.find ("d"))
-            {
-                x=3;
-            }
-            else if (souradnice.find("E") || souradnice.find ("e"))
-            {
-                x=4;
-            }
-            else if (souradnice.find("F") || souradnice.find ("f"))
-            {
-                x=5;
-            }
-            else if (souradnice.find("G") || souradnice.find ("g"))
-            {
-                x=6;
-            }
-            else if (souradnice.find("H") || souradnice.find ("h"))
-            {
-                x=7;
-            }
-            else if (souradnice.find("I") || souradnice.find ("i"))
-            {
-                x=8;
-            }
-            else if (souradnice.find("J") || souradnice.find ("j"))
-            {
-                x=9;
-            }
-            else if (souradnice.find("K") || souradnice.find ("k"))
-            {
-                x=10;
-            }
-            else if  (souradnice.find("L") || souradnice.find ("l"))
-            {
-                x=11;
-            }
-            else if (souradnice.find("M") || souradnice.find ("m"))
-            {
-                x=12;
-            }
-            else if (souradnice.find("N") || souradnice.find ("n"))
-            {
-                x=13;
-            }
-            else (souradnice.find("O") || souradnice.find ("o"));
-            {
-                x=14;
-            }
-            if(souradnice.find("14"))
-            {
-                y=14;
-            }
-            else if (souradnice.find("13"))
-            {
-                y=13;
-            }
-            else if (souradnice.find("12"))
-            {
-                y=12;
-            }
-            else if (souradnice.find("11"))
-            {
-                y=11;
-            }
-            else if (souradnice.find("10"))
-            {
-                y=10;
-            }
-            else if (souradnice.find("9"))
-            {
-                y=9;
-            }
-            else if (souradnice.find("8"))
-            {
-                y=8;
-            }
-            else if (souradnice.find("7"))
-            {
-                y=7;
-            }
-            else if (souradnice.find("6"))
-            {
-                y=6;
-            }
-            else if (souradnice.find("5"))
-            {
-                y=5;
-            }
-            else if (souradnice.find("4"))
-            {
-                y=4;
-            }
-            else if (souradnice.find("3"))
-            {
-                y=3;
-            }
-            else if (souradnice.find("2"))
-            {
-                y=2;
-            }
-            else if (souradnice.find("1"))
+            else if (souradnice.find("B") ==true || souradnice.find ("b")==true)
             {
                 y=1;
             }
-            else if (souradnice.find("0"))
+            else if (souradnice.find("C") ==true|| souradnice.find ("c")==true)
             {
-                y=0;
+                y=2;
+            }
+            else if (souradnice.find("D")==true || souradnice.find ("d")==true)
+            {
+                y=3;
+            }
+            else if (souradnice.find("E") ==true || souradnice.find ("e")==true)
+            {
+                y=4;
+            }
+            else if (souradnice.find("F")==true || souradnice.find ("f")==true)
+            {
+                y=5;
+            }
+            else if (souradnice.find("G")==true || souradnice.find ("g")==true)
+            {
+                y=6;
+            }
+            else if (souradnice.find("H") ==true || souradnice.find ("h")==true)
+            {
+                y=7;
+            }
+            else if (souradnice.find("I")==true || souradnice.find ("i")==true)
+            {
+                y=8;
+            }
+            else if (souradnice.find("J")==true || souradnice.find ("j")==true)
+            {
+                y=9;
+            }
+            else if (souradnice.find("K") ==true|| souradnice.find ("k")==true)
+            {
+                y=10;
+            }
+            else if  (souradnice.find("L")==true || souradnice.find ("l")==true)
+            {
+                y=11;
+            }
+            else if (souradnice.find("M")==true || souradnice.find ("m")==true)
+            {
+                y=12;
+            }
+            else if (souradnice.find("N") ==true|| souradnice.find ("n")==true)
+            {
+                y=13;
+            }
+            else (souradnice.find("O")==true || souradnice.find ("o")==true);
+            {
+                y=14;
+            }
+            if(souradnice.find("14")==true)
+            {
+                x=14;
+            }
+            else if (souradnice.find("13")==true)
+            {
+                x=13;
+            }
+            else if (souradnice.find("12")==true)
+            {
+                x=12;
+            }
+            else if (souradnice.find("11")==true)
+            {
+                x=11;
+            }
+            else if (souradnice.find("10")==true)
+            {
+                x=10;
+            }
+            else if (souradnice.find("9")==true)
+            {
+                x=9;
+            }
+            else if (souradnice.find("8")==true)
+            {
+                x=8;
+            }
+            else if (souradnice.find("7")==true)
+            {
+                x=7;
+            }
+            else if (souradnice.find("6")==true)
+            {
+                x=6;
+            }
+            else if (souradnice.find("5")==true)
+            {
+                x=5;
+            }
+            else if (souradnice.find("4")==true)
+            {
+                x=4;
+            }
+            else if (souradnice.find("3")==true)
+            {
+                x=3;
+            }
+            else if (souradnice.find("2")==true)
+            {
+                x=2;
+            }
+            else if (souradnice.find("1")==true)
+            {
+                x=1;
+            }
+            else if (souradnice.find("0")==true)
+            {
+                x=0;
             }
         }
         else
